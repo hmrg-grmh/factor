dd78fb7a54754bf8affb7965cdafe58efd31cc5c
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2022-01-21 16:56:08 +0800

    Update podman_toy.bash

--- podman_toy.bash
+++ podman_toy.bash
@@ -6,7 +6,7 @@ podman_app_def ()
     nn="${2:-${n}}" &&
     bash -rc "$n"' ()
     {
-        c_name="${1:-oncerun_$(date +%s%N)}" &&
+        c_name="${1:-oncerun_$(date +sec%sdot%N)}" &&
         podman run `case $# in 0) echo -ti ;; *) ;; esac
         ` --rm --name pdmrun_'"'$n'"'_"$c_name" -v "$PWD":/usr/src/"$c_name" -w /usr/src/"$c_name" '"'$nn'"' '"'$n'"' "$@" ;
     } &&
