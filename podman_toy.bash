ef2c8357d437c16587aef9b085af21b761a875fe
Hm Renga Y <68635334+hmrg-grmh@users.noreply.github.com>
2022-01-30 17:16:30 +0800

    Update podman_toy.bash
    add true meta ver by `eval`

--- podman_toy.bash
+++ podman_toy.bash
@@ -20,3 +20,26 @@ podman_app_def ()
 
 # need: Podman
 
+####
+
+podman_app_definer ()
+{
+    n="${1:-pypy}" &&
+    nn="${2:-${n}}" &&
+    eval "$n"' ()
+    {
+        c_name="${1:-oncerun_$(date +sec%sdot%N)}" &&
+        podman run `case $# in 0) echo -ti ;; *) ;; esac
+        ` --rm --name pdmrun_'"'$n'"'_"$c_name" -v "$PWD":/usr/src/"$c_name" -w /usr/src/"$c_name" '"'$nn'"' '"'$n'"' "$@" ;
+    } && echo :ok '"$n"' defined. >&2 || echo :err '"'$n'"' fail-to-define. >&2 ;
+    type declare 2>/dev/null >&2 && declare -f -- '"$n" ;
+} ;
+
+
+
+# usage:
+
+## # podman_app_definer will define fun use eval:
+## podman_app_definer pypy pypy:slim ; pypy --version
+
+# need: Podman
